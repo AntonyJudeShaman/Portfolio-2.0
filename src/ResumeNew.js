85979bb44b696ffa7cdf53fab47536b53b529e69
Antony Jude Shaman <105967620+AntonyJudeShaman@users.noreply.github.com>
2023-06-20 20:45:44 +0530

    Update ResumeNew.js

--- src/ResumeNew.js
+++ src/ResumeNew.js
@@ -2,14 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "./Particle";
-import pdf from "./FResume.pdf";
+import pdf from "./res.pdf";
 import { AiOutlineEye } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
-const resumeLink =
-  "https://www.canva.com/design/DAFcTptMqQg/xJoiGGT_mc0Nv40nKDqCLw/edit?utm_content=DAFcTptMqQg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
